8fe08091305ad58414b7dce0310eb55f3340b081
Issmuth <Ismael.mdev@gmail.com>
2024-03-22 22:14:37 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
 # installs flask of specific version
 #!/usr/bin/pup
+package { 'python':
+    ensure => '3.8.10'
+}
 
 package { 'flask':
     ensure   => '2.1.0',
+    provider => 'pip3'
+}
+
+package { 'werkzeug':
+    ensure => '2.1.1',
     provider => 'pip3'
 }
