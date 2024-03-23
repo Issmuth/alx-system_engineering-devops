4f08abaf6a1ed099a480de92e64962c17dbf6203
Issmuth <Ismael.mdev@gmail.com>
2024-03-23 15:39:08 +0300

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,8 @@
 # sets up an ssh configuration file
 
-package { 'openssh-server':
-    ensure => installed,
-}
-
 file { '/etc/ssh/sshd_config':
     ensure  => file,
     owner   => 'root',
     content => 'IdentityFile ~/.ssh/school
-                PasswordAuthentication no',
+PasswordAuthentication no',
 }
