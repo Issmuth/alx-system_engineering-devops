8fd60f52825f459153abdb0dee0015b0e6d18482
Issmuth <Ismael.mdev@gmail.com>
2024-03-28 03:42:24 +0300

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,23 +2,31 @@
 # confiures nginx
 
 package { 'nginx':
-    ensure         => installed,
-    service_manage => true,
-    nginx_configs  => {
-        'http'         => {
-            'sendfile' => 'on',
-            'server'   => {
-                'listen'                => '80',
-                'location /'            => {
-                    'return'      => '200',
-                    'add_header'  => 'Content-Type text/html',
-                    'return_body' => 'Hello World!',
-                },
-                'location /redirect_me' => {
-                    'return'     => '301',
-                    'add_header' => 'Location https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs',
-                },
-            },
-        },
-    },
+    ensure => installed,
+}
+
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.erb'),
+    notify  => Service['nginx'],
+}
+
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  location / {
+    return 200 'Hello World!';
+    add_header Content-Type text/html;
+  }
+
+  location /redirect_me {
+    return 301 'https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs';
+  }
 }
