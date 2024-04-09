8f2dd4742d78d002bdfab671db1a3e54ba9213c6
Issmuth <Ismael.mdev@gmail.com>
2024-04-09 14:03:17 +0300

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,59 +1,6 @@
 #sets up nginx server
 
-exec { 'install nginx':
-    command => '/bin/bash -c "apt-get update && apt-get install nginx"'
-}
-
-exec { 'listen http':
-    provider => shell,
-    command  => 'ufw allow "Nginx HTTP"',
-}
-
-exec { 'hello world':
-    command => '/bin/bash -c "echo \"Hello World!\" > /var/www/html/index.nginx-debian.html"',
-}
-
-exec { 'add redirect':
-    provider => shell,
-    command  => 'line1="\n\t}\n\tif (\$uri ~* \"redirect_me\") {"; line2="\t\trewrite ^ https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs permanent;"; temp=$(mktemp); echo -e "$line1" >> "$temp"; echo -e "$line2" >> "$temp"; sudo sed -i "\/^\\t\\ttry_files \$uri \$uri\/ \=404\;/r $temp" /etc/nginx/sites-available/default;',
-}
-
-exec { 'custom_error_page':
-    provider => shell,
-    command  => 'echo "Ceci n\'est pas une page" > error.html',
-}
-
-exec { 'error page config':
-    provider => shell,
-    command  => 'sudo mv error.html /var/www/html; err_line1="\n\t}\n\terror_page 404 /error.html;"',
-}
-
-exec { 'error page config 2':
-    provider => shell,
-    command  => 'err_line2="\tlocation = /error.html {"; err_line3="\t\tinternal;\n\t"',
-}
-
-exec { 'error page config 3':
-    provider => shell,
-    command  => 'errtemp=$(mktemp); echo -e "$err_line1" >> "$errtemp"',
-}
-
-exec { 'error page config 4':
-    provider => shell,
-    command  => 'echo -e "$err_line2" >> "$errtemp"',
-}
-
-exec { 'error page config 5':
-    provider => shell,
-    command  => 'echo -e "$err_line3" >> "$errtemp"',
-}
-
-exec { 'error page config 6':
-    provider => shell,
-    command  => 'sudo sed -i "\/^\\t\\ttry_files \$uri \$uri\/ \=404\;/r $errtemp" /etc/nginx/sites-available/default',
-}
-
-exec {'restart nginx':
-    provider => shell
-    command  => 'service nginx restart'
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
