78e6ad48b902708dda6fab8794e5c21d68a9e48b
issmuth <ismael.mdev@gmail.com>
2024-04-25 22:22:11 +0300

    done

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""json format export."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    employees = requests.get('https://jsonplaceholder.typicode.com/users').json()
+
+    employee_dict = {}
+    for employee in employees:
+        todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                             .format(employee.get('userId'))).json()
+
+        tasks = []
+        for task in todos:
+            task_dict = {
+                'username': employee.get('username'),
+                'task': task.get('title'),
+                'completed': task.get('completed')
+            }
+            tasks.append(task_dict)
+
+        employee_dict['{}'.format(employee.get('userId'))] = tasks
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(employee_dict, file)
