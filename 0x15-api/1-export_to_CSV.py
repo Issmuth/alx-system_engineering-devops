1b8e0179a8b7b36b98f5cfd26ca71ab8d7fc44f9
issmuth <ismael.mdev@gmail.com>
2024-04-25 20:49:38 +0300

    done

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(sys.argv[1])).json()
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1])).json()
+
+    with open('{}.csv'.format(sys.argv[1]), 'w') as file:
+        writer = csv.writer(file)
+
+        for task in todos:
+            writer.writerow(['{}'.format(sys.argv[1]),
+                             '{}'.format(employee.get('username')),
+                             '{}'.format(task.get('completed')),
+                             '{}'.format(task.get('title'))])
