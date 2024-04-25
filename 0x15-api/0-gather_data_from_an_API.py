8e46a86f76e232d26589d5c3f495bd2eeca21887
issmuth <ismael.mdev@gmail.com>
2024-04-25 16:59:51 +0300

    done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""api gather."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(sys.argv[1])).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1])).json()
+
+    done = requests.get('https://jsonplaceholder.typicode.com/'
+                        'todos?userId={}&&completed=true'
+                        .format(sys.argv[1])).json()
+
+    print('Employee {} is done with tasks ({}/{}):'
+          .format(employee.get('name'), len(done), len(todos)))
+
+    for task in done:
+        print('\t {}'.format(task.get('title')))
