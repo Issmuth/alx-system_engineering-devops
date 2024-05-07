1305fb5b326c48f9dec29947c482db399b2a70f1
issmuth <ismael.mdev@gmail.com>
2024-05-07 15:05:51 +0300

    done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Retrives the number of subscribers in a subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number of subscribers."""
+    if subreddit is None:
+        return 0
+
+    url = "https://www.reddit.com/r/{}/.json".format(subreddit)
+    header = {"user-agent": "Chrome/124.0.0.0"}
+    response = requests.get(url, headers=header)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get('data').get('children')
+    try:
+        return data[0].get('data').get('subreddit_subscribers')
+    except IndexError:
+        return 0
