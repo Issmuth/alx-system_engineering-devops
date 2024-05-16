1e6701e9161c3e6e01f755eb467db40054a5257e
issmuth <ismael.mdev@gmail.com>
2024-05-16 23:15:20 +0300

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,9 @@
 # holberton access
 
+$command1 = 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf;'
+$command2 = ' sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf'
+$complete = ${command1}${command2}
 exec {'replace-1':
   provider => shell,
-  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf;' +
-                'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command  => ${complete},
 }
