4587d02ed2d915b1b52529dcf2d8ec01b138d28f
issmuth <ismael.mdev@gmail.com>
2024-05-16 23:08:42 +0300

    done

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# holberton access
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf; sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
