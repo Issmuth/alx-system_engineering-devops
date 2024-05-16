a7bca5a6e4895058c47fd36dc551b40e0b5e0d88
issmuth <ismael.mdev@gmail.com>
2024-05-16 23:19:05 +0300

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,8 +2,8 @@
 
 $command1 = 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf;'
 $command2 = ' sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf'
-$complete = ${command1}${command2}
+$complete = "${command1}${command2}"
 exec {'replace-1':
   provider => shell,
-  command  => ${complete},
+  command  => " ${complete}",
 }
