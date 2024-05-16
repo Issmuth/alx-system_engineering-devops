8fbbd25141b7648069326e7f00584dde2d4f977c
issmuth <ismael.mdev@gmail.com>
2024-05-16 23:04:52 +0300

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# fix the request limitation
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx; sudo service nginx restart',
+}
