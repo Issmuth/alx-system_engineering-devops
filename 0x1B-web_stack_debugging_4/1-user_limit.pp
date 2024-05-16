ec5712b79036c49facf3bc3208def1284bfb2967
issmuth <ismael.mdev@gmail.com>
2024-05-16 23:12:20 +0300

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,6 @@
 
 exec {'replace-1':
   provider => shell,
-  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf; sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf;' +
+                'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
